f7f6da61b081ce7747369a61c5c284790f27129c
NikitosOS <115877783+NikitosOS@users.noreply.github.com>
2023-02-18 22:07:37 +0500

    New

---
+++ download/1.cpp
@@ -0,0 +1,10 @@
+ //Пузырёк
+ for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size-1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int b = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = b;
+            }
+        }
+    }
